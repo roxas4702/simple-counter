ff66e006320f3dccca95378b32127f904dadeadf
roxas4702 <98349290+roxas4702@users.noreply.github.com>
2022-02-22 14:26:39 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let decr = document.getElementById('decr');
 let incr = document.getElementById('incr');
+let reset = document.getElementById('reset');
 
 let num = document.getElementById('number');
 let int = 0;
@@ -14,5 +15,21 @@ function increment() {
     num.innerHTML = int;
 }
 
+function timerReset() {
+    int = 0;
+    num.innerHTML = int;
+}
+
+function wheelFunc(event) {
+    if (event.deltaY < 0) {
+        increment();
+    }
+    else if (event.deltaY > 0){
+        decrement();
+    }
+}
+
 decr.addEventListener('click', decrement);
 incr.addEventListener('click', increment);
+reset.addEventListener('click', timerReset);
+number.addEventListener('wheel', wheelFunc);
